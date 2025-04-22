5a53415fd13da7e7832d74aebbe3ee2df42b5f38
Gonçalo Matos <gmatos.ferreira@ua.pt>
2025-04-22 22:19:02 +0100

    geojson update

--- js/db.js
+++ js/db.js
@@ -1,5 +1,5 @@
 // Global attributes
-const BASE_URL = "https://lpp.gmatos.pt/rede-madrugada/geojson";
+const BASE_URL = "/rede-madrugada/geojson";
 const DB_OPERATORS = {
     Carris: { color: '#003f8f', name: 'Carris Municipal' },
     CarrisMetropolitana: { color: '#ffdd01', name: 'Carris Metropolitana' },
